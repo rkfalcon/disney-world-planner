11555f04c8105ff291b07934ea44e29e7f66f577
rkfalcon <rkfalcon@gmail.com>
2026-03-23 16:48:44 -0400

    feat: update

---
+++ supabase/update_attractions.sql
@@ -0,0 +1,11 @@
+-- Update Disney World attractions with complete data
+-- Run this if you already have the tables created
+
+-- Clear existing attractions and re-seed
+DELETE FROM itinerary_items;
+DELETE FROM dining_reservations;
+DELETE FROM itinerary_days;
+DELETE FROM trips;
+DELETE FROM attractions;
+DELETE FROM parks;
+
